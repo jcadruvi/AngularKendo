17027ac16bd9b06cfaf007e2f3fbf5fb7e79bc94
Josh Cadruvi <jcadruvi78@yahoo.com>
2014-01-12 10:41:40 -0800

    Added the angular app and directives.

---
+++ AngularKendo.Site/Scripts/kendoComboBox.js
@@ -0,0 +1,62 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').directive('kendoComboBox', kendoComboBox);
+
+    function kendoComboBox() {
+        var self = {};
+
+        self.restrict = 'A';
+        self.scope = {
+            selectData: '=',
+            onSelect: '=',
+            mData: '=',
+            value: '=',
+            width: '='
+        };
+
+        self.link = function (scope, element, attrs) {
+            var data,
+                dataTextField = 'description',
+                dataValueField = 'id',
+                width = '88%';
+
+            $(element[0]).kendoComboBox({
+                change: function () {
+                    if (scope.onSelect) {
+                        scope.onSelect(scope.selectData, this.value(), this.text());
+                    }
+                },
+                dataTextField: dataTextField,
+                dataValueField: dataValueField,
+                dataSource: {
+                    data: scope.mData
+                },
+                filter: "contains",
+                suggest: true,
+                value: scope.value
+            });
+            data = $(element[0]).data('kendoComboBox');
+            scope.$watch('mData', function () {
+                if (scope.mData.length > 0) {
+                    data.dataSource.data(scope.mData);
+                    data.value(scope.value);
+                }
+            });
+            scope.$watch('value', function () {
+                if (scope.mData.length > 0) {
+                    data.value(scope.value);
+                }
+            });
+
+            if (scope.width) {
+                width = scope.width;
+            }
+            $(element[0]).closest(".k-widget").find(".k-dropdown-wrap").css("width", width);
+            $(element[0]).closest(".k-widget").css("width", width);
+        };
+
+        return self;
+    }
+
+})();
---
+++ AngularKendo.Site/Scripts/kendoDatePicker.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').directive('mobDatePicker', mobitorDatePicker);
+
+    function mobitorDatePicker() {
+        var self = {};
+
+        self.restrict = 'A';
+        self.scope = {
+            selectData: '=',
+            onSelect: '=',
+            value: '='
+        };
+        self.link = function (scope, element, attrs) {
+            var data;
+            $(element[0]).kendoDatePicker({
+                change: function () {
+                    if (scope.onSelect) {
+                        scope.onSelect(scope.selectData, this.value());
+                    }
+                },
+                format: 'MM/dd/yyyy',
+                value: scope.value
+            });
+            data = $(element[0]).data('kendoDatePicker');
+            scope.$watch('value', function () {
+                if (scope.value) {
+                    data.value(new Date(scope.value));
+                }
+            });
+        };
+
+        return self;
+    }
+
+})();

77183b536bbae2f10506c1eab8609d3e3b11df27
Josh Cadruvi <jcadruvi78@yahoo.com>
2014-05-14 20:25:57 -0700

    Starting to fix the date picker controls.

--- AngularKendo.Site/Scripts/kendoDatePickerDirective.js
+++ AngularKendo.Site/Scripts/kendoDatePickerDirective.js
@@ -11,15 +11,19 @@
             value: '='
         };
         self.link = function (scope, element, attrs) {
-            var data;
-            $(element[0]).kendoDatePicker({
+            var $element = $(element[0]),
+                data;
+            $element.kendoDatePicker({
                 change: function () {
                     scope.value = this.value();
                     scope.$apply();
                 },
                 format: 'd'
             });
-            data = $(element[0]).data('kendoDatePicker');
+            data = $element.data('kendoDatePicker');
+            scope.$watch('value', function () {
+                data.value(new Date(scope.value));
+            });
         };
 
         return self;
